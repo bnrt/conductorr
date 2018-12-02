5dde1bdc1f2112bd90d7bae64fafffad451e8650
Bonaparte <45331367+bnrt@users.noreply.github.com>
2018-12-02 12:20:37 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
 @echo off
 
-copy "%~dp0src\engine.js" + "%~dp0src\providers\supported\*.js" + "%~dp0src\providers\community\*.js" + "%~dp0src\render.js" "%~dp0conductorr.user.js"
+type "%~dp0src\engine.js" > "%~dp0conductorr.user.js"
+type "%~dp0src\providers\supported\*.js" >> "%~dp0conductorr.user.js"
+type "%~dp0src\providers\community\*.js" >> "%~dp0conductorr.user.js"
+type "%~dp0src\render.js" >> "%~dp0conductorr.user.js"
